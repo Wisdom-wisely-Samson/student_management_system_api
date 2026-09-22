0372c4e3dce3140f01c2c707f33e588e866467d8
Wisdom-wisely-Samson <wiselywisdom4@gmail.com>
2026-09-22 08:28:37 +0300

    feat: improve authentication and authorization flow

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
+from app.models.users import User
+from app.auth_dependency import get_current_user
 
 from app.database import get_db
 from app.schemas.user import UserCreate, UserResponse, UserLogin, TokenResponse, StudentRegister, StudentAccountResponse
@@ -20,6 +22,18 @@ def register_student(student_data: StudentRegister, db: Session = Depends(get_db
         "user": new_user,
         "student": new_student
     }
+@router.get("/me", response_model=UserResponse)
+def get_my_profile(db: Session = Depends(get_db), current_user: User =Depends(get_current_user)):
+    return current_user
+
+@router.get("/student/me")
+def get_my_student_profile(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    if not current_user.student:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Student profile not found!"
+        )
+    return current_user.student
 
 @router.post("/login", response_model=TokenResponse)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
